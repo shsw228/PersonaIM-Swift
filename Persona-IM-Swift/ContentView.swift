b3e9238f86d9a1f06ed5096a1c39984781b2c53f
Tkng <13506491+shsw228@users.noreply.github.com>
2024-09-13 22:14:13 +0900

    init commit

---
+++ Persona-IM-Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Persona-IM-Swift
+//  
+//  Created by shsw228 on 2024/09/13
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Persona-IM-Swift/Persona_IM_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Persona_IM_SwiftApp.swift
+//  Persona-IM-Swift
+//  
+//  Created by shsw228 on 2024/09/13
+//
+
+import SwiftUI
+
+@main
+struct Persona_IM_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
