676e38d0201dce036e5c25f6c997c2b4157ecb9d
Tkng <13506491+shsw228@users.noreply.github.com>
2024-09-13 22:55:27 +0900

    add resources

--- Persona-IM-Swift/ContentView.swift
+++ Persona-IM-Swift/ContentView.swift
@@ -9,16 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.personaRed
+                .ignoresSafeArea()
         }
-        .padding()
     }
+
 }
 
 #Preview {
     ContentView()
+    
 }
